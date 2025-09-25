59af691068f5241da5009d25c8f65e029a45846c
Manikandan1511 <manikandanmk15112005@gmail.com>
2025-09-25 12:02:53 +0530

    2635. Apply Transform Over Each Element in Array --> easy problem

---
+++ 2635-Apply_Transform_Over_Each_Element_in_Array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        const result = fn(arr[i], i);
+        newArr.push(result);
+    }
+    return newArr;
+};
